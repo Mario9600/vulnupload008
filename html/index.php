ffb06e74e1d933884792074fc5b7dca193429823
Mario9600 <mario12-@Live.nl>
2023-04-21 14:20:00 +0200

    Add files via upload

---
+++ html/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="shortcut icon" href="img\logo.png" />
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Home Page</title>
+</head>
+<body>
+  <header>
+    <div class="logo">
+      <a href="index.php">
+        <img src="img\logo.png" alt="Company Logo">
+      </a>
+    </div>
+    <div class="navigation">
+      <a href="login.php"><button>Log In</button></a>
+    </div>
+  </header>
+
+  <div class="container">
+    <h1>Welcome to our Insurance Company</h1>
+    <p>Here at our insurance company, we offer a wide range of services to help protect you and your assets. From auto and home insurance to life and health insurance, we've got you covered.</p>
+    <p>Please use the links above to manage your client data and upload invoices.</p>
+    <a href="login.php" class="btn">Manage Clients</a>
+    <a href="Invoices.php" class="btn">Upload Invoices</a>
+  </div>
+
+  <footer>
+    <p>&copy; 2023 Insurance Company</p>
+  </footer>
+</body>
+</html>
